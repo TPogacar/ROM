f97ea084d3eac3d192d234c83ccb7b4357e61a01
TPogacar <39917129+TPogacar@users.noreply.github.com>
2019-02-08 16:11:39 +0100

    zadnje vaje.. približno

---
+++ matlab_octave/ali_je_sedlo.m
@@ -0,0 +1,9 @@
+%funkcija ali_je_sedlo preveri, ali ima matrika A na mestu (i, j) sedlo ali ne
+
+function r = ali_je_sedlo(A, i, j)
+  minVrstice = min(A(i, :));
+  maxStolpca = max(A(:, j));
+  maxVrstice = max(A(i, :));
+  minStolpca = min(A(:, j));
+  r = (A(i,j) == minVrstice && A(i,j) == maxStolpca) || (A(i,j) == maxVrstice && A(i,j) == minStolpca);
+endfunction
